0eb5381e15cdbd3d4873bcf705a7b6f0546a769b
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2026-02-03 16:50:54 -0800

    imports

--- frontend/src/pages/TicketDetail.jsx
+++ frontend/src/pages/TicketDetail.jsx
@@ -1,3 +1,10 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function TicketDetail() {
-  return <div>Ticket Detail View</div>;
+  return (
+    <div>
+      Ticket Detail View
+    </div>
+  );
 }
