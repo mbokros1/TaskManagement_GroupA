5a6cf9bcdf21bce96f7a99ad5417714ce9f44d22
Sealshusky <129021061+Sealshusky@users.noreply.github.com>
2026-02-03 16:51:15 -0800

    Add useParams to TicketDetail component

--- frontend/src/pages/TicketDetail.jsx
+++ frontend/src/pages/TicketDetail.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function TicketDetail() {
+  const { id } = useParams();
+
   return (
     <div>
       Ticket Detail View
