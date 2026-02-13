c16d1b99005eaf03773c7b83f688aba52da620a0
Ann N <jovyannnelson@gmail.com>
2026-02-12 17:21:13 -0800

    Add React in App.jsx and App.test.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-
+import React from 'react';
 import { Button, CircularProgress, Typography, Box } from '@mui/material';
 import useAuth from './auth/useAuth.js';
 import AdminDashboard from './dashboard/AdminDashboard.jsx';
